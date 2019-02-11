f261bbb7382069d551b8815a4e9e04411f4d52ec
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2019-02-11 16:36:50 +0100

    dialog delete document

--- src/app/document/document-list/document-list.component.ts
+++ src/app/document/document-list/document-list.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {MatDialog} from '@angular/material';
 
 import { DocumentService } from '../shared/document.service';
 import { AppSnackbarService } from '../../shared/app-snackbar.service';
+import { GenerecDialogComponent } from '../../generec-dialog/generec-dialog.component';
+import { Document } from '../../shared/document.model';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent implements OnInit {
+export class DocumentListComponent {
 
   @Input() documents: Document[] = [];
   @Output() selectDocument = new EventEmitter<number>();
@@ -16,13 +19,24 @@ export class DocumentListComponent implements OnInit {
   @Output() renameDocumentChange = new EventEmitter<Document>();
 
   constructor(private documentService: DocumentService,
-  private appSnackbarService: AppSnackbarService) { }
-
-  ngOnInit() {
-  }
+  private appSnackbarService: AppSnackbarService,
+  private dialog: MatDialog) { }
 
   onSelectDocument(id){
     this.selectDocument.emit(id);
+  }
+
+  openDialogDeleteDocument(document: Document){
+    const dialogRef = this.dialog.open(GenerecDialogComponent, {
+     width: '350px',
+     data: {title: 'Delete Document', message: "Do you want to delete the document: " + document.name +" ?"}
+   });
+
+   dialogRef.afterClosed().subscribe(result => {
+     if(result){
+       this.deleteDocument(document.id);
+     }
+   });
   }
 
   deleteDocument(id){
