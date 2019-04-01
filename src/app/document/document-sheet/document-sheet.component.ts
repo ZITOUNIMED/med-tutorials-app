dfc692a768d770b257905fe000f37cf8208157aa
Zitouni mohamed <zitouni.mohamed.email@gmail.com>
2019-04-01 17:16:51 +0200

    export as pdf

--- src/app/document/document-sheet/document-sheet.component.ts
+++ src/app/document/document-sheet/document-sheet.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 import { Document } from '../shared/model/document.model';
 import { Element } from '../shared/model/element.model';
 import {ActivatedRoute} from '@angular/router';
 import {filter, first, map} from 'rxjs/internal/operators';
+import {ElementType} from '../shared/element-type';
 
 @Component({
   selector: 'app-document-sheet',
@@ -61,5 +63,35 @@ export class DocumentSheetComponent implements OnInit {
 
   onCancelChange(cancel) {
     this.shouldCancelChanges = cancel;
+  }
+
+  exportAsPdf() {
+    const doc = new jsPDF();
+    let page = 0;
+    this.document.elements.sort((e1, e2) => (e1.row - e2.row) + (e1.page - e2.page) * 100);
+    const titlesMap = new Map();
+    titlesMap.set(ElementType.BIG_TITLE, 'h1');
+    titlesMap.set(ElementType.MEDIUM_TITLE, 'h2');
+    titlesMap.set(ElementType.SMALL_TITLE, 'h3');
+    titlesMap.set(ElementType.VERY_SMALL_TITLE, 'h4');
+    do {
+      if ( page > 0 ) {
+        doc.addPage();
+      }
+      const elements = this.document.elements.filter(e => e.page === page);
+      elements.sort((e1, e2) => (e1.row - e2.row));
+      elements.forEach(element => {
+        const titletag = titlesMap.get(element.type);
+        if (titletag) {
+          doc.fromHTML(`<${titletag}>${element.text}</${titletag}>`, element.row * 10 + 20);
+        } else if (element.type === ElementType.SOURCE_CODE) {
+          doc.fromHTML(`<textarea>${element.text}</textarea>`, element.row * 10 + 20);
+        } else {
+          doc.text(element.text, 20, element.row * 10 + 20);
+        }
+      });
+      page++;
+    } while ( this.document.elements.some(e => e.page >= page) );
+    doc.save(this.document.name + '.pdf');
   }
 }
