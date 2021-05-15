b9a28ea181532b5621e369782990bed78c1b3d9e
Mohamed Zitouni <zitouni.mohamed.email@gmail.com>
2021-05-15 23:57:37 +0200

    download all document

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -15,9 +15,20 @@ export class ManagementComponent implements OnInit {
 
   exportDocumentsAsJson(){
     this.service.exportDocumentsAsJson()
-    .subscribe(res => {
-      console.log(res);
-    })
+    .subscribe(data => this.downloadFile(JSON.stringify(data))),
+                 error => console.log('Error downloading the file.'),
+                 () => console.info('OK');
   }
 
+  downloadFile(data) {
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+
+    const blob = new Blob([data], { type: 'text/json' });
+
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = "allAocuments.json";
+    a.click();
+  }
 }
